ba04182c55a27ce5bf9b1d8e871c71249e03c85e
yeoseong_yoon <yeoseong_yoon@daum.net>
2019-02-22 13:18:28 +0900

    2019/02/22 yeoseong_yoon kivescript-cloud-config server commit

--- kivescript-boot/src/main/java/com/korea/kivescript/KivescriptBootApplication.java
+++ kivescript-boot/src/main/java/com/korea/kivescript/KivescriptBootApplication.java
@@ -1,13 +1,33 @@
 package com.korea.kivescript;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KivescriptBootApplication {
-
+public class KivescriptBootApplication implements CommandLineRunner{
+	
+	private static Logger logger = LoggerFactory.getLogger(KivescriptBootApplication.class);
+	
+	@Value("${kivescript.version}")
+	private String kiveScriptVersion;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KivescriptBootApplication.class, args);
 	}
 
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("=================================");
+		logger.info("== kivescript's version is {} =",kiveScriptVersion);
+		logger.info("=================================");
+	}
+	
 }
