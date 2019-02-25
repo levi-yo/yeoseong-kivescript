5f4596add4b131afa3387bb4007c7171a4d75f4f
yeoseong_yoon <yeoseong_yoon@daum.net>
2019-02-25 13:27:17 +0900

    kivescript config commit

---
+++ kivescript-boot/src/main/java/com/korea/kivescript/KiveScriptStartUp.java
@@ -0,0 +1,16 @@
+package com.korea.kivescript;
+
+
+/**
+ * 애플리케이션 구동시 오브젝트 핸들러가 잘 로드됬는지 확인하는 클래스
+ * @author yun-yeoseong
+ *
+ */
+public class KiveScriptStartUp {
+	
+	
+	public String startUpMacro() {
+		return "javascript engine alive !";
+	}
+	
+}
--- kivescript-boot/src/main/java/com/korea/kivescript/KivescriptBootApplication.java
+++ kivescript-boot/src/main/java/com/korea/kivescript/KivescriptBootApplication.java
@@ -1,8 +1,6 @@
 package com.korea.kivescript;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.korea.kivescript.lang.javascript.JavaScriptHandler;
 
 @SpringBootApplication
 public class KivescriptBootApplication implements CommandLineRunner{
@@ -32,13 +32,17 @@ public class KivescriptBootApplication implements CommandLineRunner{
 		logger.info("==================================");
 		logger.info("== kivescript's version is {} ==",kiveScriptVersion);
 		logger.info("==================================");
-		RiveScript riveScript = new RiveScript(Config.utf8());
-		
+		RiveScript riveScript = new RiveScript(Config.newBuilder()
+													 .utf8(true)
+													 .concat(ConcatMode.SPACE)
+													 .build());
+		riveScript.setHandler("javascript", new JavaScriptHandler());
 		riveScript.loadFile(RiveScript.class.getClassLoader().getResource("rive/startup.rive").getFile());
 		riveScript.sortReplies();
-		logger.info("=========================================");
+		logger.info("===================================================================");
 		logger.info("== knock knock to bot -> \"{}\" ==",riveScript.reply("yeoseong", "kivescript startup test"));
-		logger.info("=========================================");
+		logger.info("===================================================================");
+		System.out.println(riveScript.reply("yeoseong", "kivescript startup test"));
 	}
 	
 }
